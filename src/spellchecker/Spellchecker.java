ad49bdd0b719b84b4559f9bc27a761ec454db779
Jared Cassarly <jaredcassarly@case.edu>
2016-11-29 11:07:15 -0500

    created netbeans project

---
+++ src/spellchecker/Spellchecker.java
@@ -0,0 +1,16 @@
+package spellchecker;
+
+/**
+ *
+ * @author Jared Cassarly (jwc160)
+ */
+public class Spellchecker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
