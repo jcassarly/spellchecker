caced96e45b1eedb87d7b70f8694e93c15c7d155
Jared Cassarly <jaredcassarly@case.edu>
2016-11-30 09:52:57 -0500

    Spell checker is working
    need to add support for apostrophes

--- src/spellchecker/Dictionary.java
+++ src/spellchecker/Dictionary.java
@@ -1,8 +1,10 @@
 package spellchecker;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
@@ -11,50 +13,74 @@ import java.util.Scanner;
 public class Dictionary {
     
     private Node root;
+    private int size;
+    private int errors;
     
     /**
      * Load in a text file that contains a dictionary - one word on each line
      * @param textFile the dictionary with one word per line
      * @throws java.io.FileNotFoundException yup this happens if you give bad input - fight me lol // i should probably take this line out
      */
-    public Dictionary(String textFile) throws FileNotFoundException {
+    public Dictionary(File textFile) throws IOException {
         root = new Node();
+        size = 0;
+        errors = 0;
         load(textFile);
     }
     
-    private void load(String textFile) throws FileNotFoundException {
-        Scanner in = new Scanner(textFile);
+    private void load(File textFile) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(textFile));
         
-        while (in.hasNextLine()) {
-            String s = in.nextLine();
+        while (in.ready()) {
+            String s = in.readLine();
             if (!s.equals("\n")) {
-                this.insert(s);
+                boolean noError = this.insert(s);
+                if (!noError) {
+                    System.out.println(s);
+                    errors++;
+                }
+                size++;
             }
         }
         
         in.close();
     }
     
-    public void insert(String newWord) {
+    public boolean insert(String newWord) {
         Node current = root;
+        // run through the word to make sure all characters are valid
+        boolean charsValid = true;
         for (int i = 0; i < newWord.length(); i++) {
             int index = getIndex(newWord.charAt(i));
-            
-            // if the current character is not the last one
-            if (i < newWord.length() - 1) {
-                // go to the next node in the trie
-                // if the next node is null, create one
-                if (current.nexts[index] == null) {
-                    current.nexts[index] = new Node();
-                }
-                // set current to next
-                current = current.nexts[index];
-            }
-            // if the current character is the last idea in the string
-            else {
-                current.wordExists[index] = true;
+            if (index < 0 || index > 25) {
+                charsValid = false;
+                i = newWord.length();
             }
         }
+        // if all the characters are valid, insert the word
+        if (charsValid) {
+            for (int i = 0; i < newWord.length(); i++) {
+                int index = getIndex(newWord.charAt(i));
+
+                // if the current character is not the last one
+                if (i < newWord.length() - 1) {
+                    // go to the next node in the trie
+                    // if the next node is null, create one
+                    if (current.nexts[index] == null) {
+                        current.nexts[index] = new Node();
+                    }
+                    // set current to next
+                    current = current.nexts[index];
+                }
+                // if the current character is the last idea in the string
+                else {
+                    current.wordExists[index] = true;
+                }
+            }
+            return true;
+        }
+        // if the characters were not valid, show that an error has occured
+        else return false;
     }
     
     public boolean checkDictionary(String word) {
@@ -77,6 +103,14 @@ public class Dictionary {
     
     private int getIndex(char c) {
         return c - 'a';
+    }
+    
+    public int getSize() { 
+        return size;
+    }
+    
+    public int getErrors() {
+        return errors;
     }
     
     class Node {
--- src/spellchecker/Spellchecker.java
+++ src/spellchecker/Spellchecker.java
@@ -1,6 +1,7 @@
 package spellchecker;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
 
 /**
  *
@@ -11,10 +12,11 @@ public class Spellchecker {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException {
-        String dict = "a\nabs\ncandy";
-        Dictionary d = new Dictionary(dict);
-        System.out.println(d.checkDictionary("burrito"));
+    public static void main(String[] args) throws IOException {
+        //String dict = "a\nabs\ncandy";
+        Dictionary d = new Dictionary(new File("src/resources/dictionary84000.txt"));
+        System.out.println(d.checkDictionary("gerrymander"));
+        System.out.println(d.getErrors());
     }
     
 }
